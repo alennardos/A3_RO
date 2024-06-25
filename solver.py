fcc78bdd73debd19dbeb7c552516664612d54fa0
alen amirbekyan <alenamirbekyan18@gmail.com>
2024-06-25 10:23:15 +0200

    solver

--- solver.py
+++ solver.py
@@ -1,37 +1,48 @@
 from pulp import *
+import random
+from math import sqrt
+import matplotlib.pyplot as plt
 
-AB = LpVariable("AB", 0, 1, const.LpBinary)
-AC = LpVariable("AC", 0, 1, const.LpBinary)
-AD = LpVariable("AD", 0, 1, const.LpBinary)
-AE = LpVariable("AE", 0, 1, const.LpBinary)
+liste = [(25, 11), (22, 24), (25, 6), (28, 27), (24, 22), (16, 2), (26, 1), (5, 22), (3, 19), (13, 9)]
 
-BC = LpVariable("BC", 0, 1, const.LpBinary)
-BD = LpVariable("BD", 0, 1, const.LpBinary)
-BE = LpVariable("BE", 0, 1, const.LpBinary)
+# for i in range(10):
+#     liste.append((random.randint(0, 30), random.randint(0, 30)))
 
-CD = LpVariable("CD", 0, 1, const.LpBinary)
-CE = LpVariable("CE", 0, 1, const.LpBinary)
 
-DE = LpVariable("DE", 0, 1, const.LpBinary)
+def calculeDistance(val):
+    return sqrt((val[0][0] - val[1][0])**2  + (val[0][1] - val[1][1])**2)
 
-# probleme
-prob = LpProblem("choix", LpMinimize)
+prob = LpProblem("MonPb", LpMinimize)
 
-# objectif
-prob += AB + AC + AD + AE == 2
-prob += AB + BC + BD + BE == 2
-prob += AC + BC + CD + CE == 2
-prob += AE + BE + CE + DE == 2
-prob += AD + BD + CD + DE == 2
 
-# contrainte
-prob += 10*AB + 2*AC + 3*AD + 4*AE + 1*BC + 5*BD + 30*BE + 5*CD + 8*CE + 9*DE
+x = LpVariable.dicts('objects', [(liste[i],liste[j]) for i in range(len(liste)) for j in range(i+1, len(liste))]
+                             ,lowBound = 0
+                             ,upBound = 1
+                             ,cat = 'Binary'
+                             )
+
+
+prob += lpSum([calculeDistance((liste[i],liste[j]))*x[(liste[i],liste[j])] for i in range(len(liste)) for j in range(i+1, len(liste))])
+
+for i in range(len(liste)):
+    prob += lpSum([x[(liste[i],liste[j])] for j in range(len(liste)) if (liste[i],liste[j]) in x.keys()]) + lpSum([x[(liste[j],liste[i])] for j in range(len(liste)) if (liste[j],liste[i]) in x.keys()]) == 2
+
+for i in range(len(liste)):
+    for j in range(i, len(liste)):
+        if (liste[i],liste[j]) in x.keys():
+            for k in range(j, len(liste)):
+                if (liste[i],liste[k]) in x.keys() and (liste[j],liste[k]) in x.keys():
+                    prob += x[liste[i],liste[j]] + x[liste[i],liste[k]] + x[liste[j],liste[k]] <= 2
 
 prob.solve()
-print("a")
+
 print(LpStatus[prob.status])
 print("Min=", value(prob.objective))
 
-# variables resultat
-for v in prob.variables():
-    print("%s=%.2f"%(v.name,v.varValue), end=', ')
+print("Valeur: " + str(value(prob.objective)))
+for i in range(len(liste)):
+    for j in range(i+1, len(liste)):
+        if x[liste[i],liste[j]].value() == 1.0:
+            plt.plot((liste[i][0], liste[j][0]), (liste[i][1], liste[j][1]), marker = 'o')
+
+plt.show()
